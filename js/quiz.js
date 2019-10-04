e6be30a1d4d9f82093d16fc01e4f5567c48e7b0e
Daniel H <dhenkes95@icloud.com>
2019-10-04 14:48:33 +0000

    Updated code

--- js/quiz.js
+++ js/quiz.js
@@ -3,6 +3,23 @@ var questionsAnswers = [
   ['How many continents are in the world?', 7],
   ['Which team did the Chicago Cubs play in the 1945 World Series?', 'Detroit Tigers']
 ]
+var answer=' ';
+var correct=' ';
+var incorrect=' ';
 function print(message) {
   document.write(message);
 }
+
+for ( i = 0; i < questionsAnswers.length; i += 1) {
+  answer = prompt(questionsAnswers[i][0]);
+  answer.toLowerCase();
+  if (answer === questionsAnswers[i][1]) {
+    correct += questionsAnswers[i][0];
+  } else {
+    incorrect += questionsAnswers[i][0];
+  }
+}
+document.write('<h1><strong>You got the following questions right: </strong></h1>');
+print(correct);
+document.write('<h1><strong>You got the following questions wrong: </strong></h1>');
+print(incorrect);
