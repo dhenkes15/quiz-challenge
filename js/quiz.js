267db2912addfcd246114ce9cd99cfbf4dc5f900
Daniel H <dhenkes95@icloud.com>
2019-10-04 14:13:10 +0000

    Add initial files

---
+++ js/quiz.js
@@ -0,0 +1,8 @@
+var questionsAnswers = [
+  ['What is the capital of texas?', 'Austin'],
+  ['How many continents are in the world?', 7],
+  ['Which team did the Chicago Cubs play in the 1945 World Series?', 'Detroit Tigers']
+]
+function print(message) {
+  document.write(message);
+}
